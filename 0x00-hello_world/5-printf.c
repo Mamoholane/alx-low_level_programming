134a6f22d3a07d053ebad654dbbcd1e52b8df57a
Mamoholane <mamoholanemogotlane@gmailcom>
2023-03-16 20:57:25 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
+Task 3.txt
 #include <stdio.h>
 
 /**
-  * main - print the string in the pu function
-  *
-  * Description: using the main function
-  * This program prints with proper grammar, but the outcome is a piece of art,
-  * Return: (0)
-  */
-int main()void
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
 {
-        print ("with proper grammar, but the outcome is a piece of art,\n");
-	return: (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-
