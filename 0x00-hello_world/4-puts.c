0256ab4daad1281299564a7db7510083d7882d68
Mamoholane <mamoholanemogotlane@gmailcom>
2023-03-16 19:46:54 -0700

    My first c programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/*/**
+ * main - print the string in the put function
+ * *
+ * *Description: using the main function
+ * *This Programming is like building a multilingual puzzle
+ * *Return:0
+ * */
+iiint main(void)
+{[{
+        puts("\"Programming is like building a multilingual puzzle");
+Re      return: (0);
+
+  }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - print the string in the pu function
+  *
+  * Description: using the main function
+  * This program prints with proper grammar, but the outcome is a piece of art,
+  * Return: (0)
+  */
+int main()void
+{
+        print (*with proper grammar, but the outcome is a piece of art,\n*);
+return: (0);
+}
+
