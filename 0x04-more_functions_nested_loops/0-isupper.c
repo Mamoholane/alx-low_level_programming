b3d3cc244b66681134cf68fce949f339e93c9064
Mamoholane <mamoholanemogotlane@gmailcom>
2023-03-23 14:56:21 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,23 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
 /**
- * main - header file for protoypes
- * void protoypes (void)
- * int protoypes (int)
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
-int _putchar(char);
-int _isupper(int);
-int _isdigit(int);
-int mul(int, int);
-
-#endif /* MAIN_H */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
